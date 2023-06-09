15b78a55c892435ab0bc80e70040a3763cf04c6e
Manon <manonlefkos@gmail.com>
2023-06-09 10:06:15 +0200

    add contains function to linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -31,80 +31,93 @@ export default class LinkedList {
 
   size() {
     let node = this.listHead;
-    let total = 0
+    let total = 0;
     while (node) {
-      total += 1
-      node = node.nextNode
+      total += 1;
+      node = node.nextNode;
     }
-    return total
+    return total;
   }
 
   head() {
-    return this.listHead
+    return this.listHead;
   }
 
   tail() {
-    let node = this.listHead
+    let node = this.listHead;
     while (node.nextNode) {
-      node = node.nextNode
+      node = node.nextNode;
     }
-    return node
+    return node;
   }
 
   at(index) {
-    let current = 0
-    let node = this.listHead
+    let current = 0;
+    let node = this.listHead;
     while (current < index) {
-      node = node.nextNode
-      current += 1
+      node = node.nextNode;
+      current += 1;
     }
-    return node
-  }
-
-  toString() {
-    let node = this.listHead
-    let consoleMsg = ""
-    while (node) {
-      consoleMsg += `( ${node.value} ) => `
-      node = node.nextNode
-    }
-    consoleMsg += `null`
-    console.log(consoleMsg)
+    return node;
   }
 
   pop() {
     let current = this.listHead;
     let previous;
     while (current.nextNode) {
-      previous = current
-      current = current.nextNode
+      previous = current;
+      current = current.nextNode;
     }
-    previous.nextNode = null
+    previous.nextNode = null;
+  }
+
+  contains(value) {
+    let node = this.listHead;
+    while (node.nextNode) {
+      if (node.value === value) return true;
+      node = node.nextNode;
+    }
+    // last list item
+    if (node.value === value) return true;
+    return false;
+  }
+
+  toString() {
+    let node = this.listHead;
+    let consoleMsg = "";
+    while (node) {
+      consoleMsg += `( ${node.value} ) => `;
+      node = node.nextNode;
+    }
+    consoleMsg += `null`;
+    console.log(consoleMsg);
   }
 }
 
 const list = new LinkedList();
 list.append("index 1 node");
 list.append("index 2 node");
-list.append("index 3 node")
-list.append("index 4 node")
-list.append("index 5 node")
-list.append("index 6 node")
+list.append("index 3 node");
+list.append("index 4 node");
+list.append("index 5 node");
+list.append("index 6 node");
 
 list.prepend("new head");
 
 console.log("list: ", list.listHead);
 
-console.log("head: ", list.head())
+console.log("head: ", list.head());
 
-console.log("tail: ", list.tail())
+console.log("tail: ", list.tail());
 
-console.log("total items: ", list.size())
+console.log("total items: ", list.size());
 
-console.log("index 3: ", list.at(3))
+console.log("index 3: ", list.at(3));
 
-list.toString()
+list.toString();
 
 // remove last item
-list.pop()
-list.toString()
+list.pop();
+list.toString();
+
+console.log(list.contains("index 5 node"));
